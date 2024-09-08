f1bf49c6b8332a8e0a43351ffaa308514d27e0c3
LupoArticoblu <wolf94-@libero.it>
2024-09-08 13:40:13 +0200

    implementazione altre classi enemy

--- script.js
+++ script.js
@@ -2,12 +2,23 @@
 //utilizzando il seguente commento otteremo dei suggerimenti per ctx sui metodi dei canvas
 /** @type {HTMLCanvasElement} */
 const canvas = document.getElementById('canvas1');
+const canvas2 = document.getElementById('canvas2');
+const canvas3 = document.getElementById('canvas3');
+const canvas4 = document.getElementById('canvas4');
+const canvases = [canvas, canvas2, canvas3, canvas4];
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width = 400;
-const CANVAS_HEIGHT = canvas.height = 1000;
+const ctx2 = canvas2.getContext('2d');
+const ctx3 = canvas3.getContext('2d');
+const ctx4 = canvas4.getContext('2d');
+const CANVAS_WIDTH = 400;
+const CANVAS_HEIGHT = 1000;
 
 const numberOfEnemies = 60;
 const enemyArr = [];
+const enemy2Arr = [];
+const enemy3Arr = [];
+const enemy4Arr = [];
+
 
 // const enemyImage1 = new Image();
 // enemyImage1.src = './NPC enemy/enemy1.png';
@@ -46,7 +57,9 @@ class Enemy {
     this.width = this.spriteWidth / 2.5;
     this.height = this.spriteHeight / 2.5;
     this.x = Math.random() * (CANVAS_WIDTH - this.width);
-    this.y = Math.random() * (CANVAS_HEIGHT - this.height);
+    this.initialY = Math.random() * (CANVAS_HEIGHT - this.height);
+    this.y = this.initialY;
+    //this.y sarà configurabile quindi usiamo il metodo Object.defineProperty
     this.frameX = 0;
     this.WingsDifferentFly = Math.floor(Math.random() * 3) + 1;
   }
@@ -70,21 +83,113 @@ class Enemy {
   }
 };
 
+class Enemy2 extends Enemy {
+  constructor() {
+    super();
+    this.Image = new Image();
+    this.Image.src = './NPC enemy/enemy2.png';
+    this.speed = Math.random() * 4 + 1; // Velocità maggiore di 1 per movimento visibile
+    this.spriteWidth = 266;
+    this.spriteHeight = 188;
+    this.width = this.spriteWidth / 2.5;
+    this.height = this.spriteHeight / 2.5;
+    this.WingsDifferentFly = Math.floor(Math.random() * 3) + 1;
+  }
+  
+  update() {
+    super.update();
+    // Muove i nemici verso sinistra
+    this.x -= this.speed;
+    this.y = this.initialY;
+    // Se escono fuori dallo schermo, li facciamo rientrare
+    if (this.x + this.width < 0) {
+      this.x = CANVAS_WIDTH;
+    }
+  }
+
+  draw() {
+    
+    ctx2.drawImage(this.Image, this.frameX * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y , this.width, this.height);
+  }
+}
+
+class Enemy3 extends Enemy{
+  constructor(){
+    super();
+    this.Image = new Image();
+    this.Image.src = './NPC enemy/enemy3.png';
+    this.spriteWidth = 218;
+    this.spriteHeight = 177;
+    this.width = this.spriteWidth / 2.5;
+    this.height = this.spriteHeight / 2.5;
+    this.WingsDifferentFly = Math.floor(Math.random() * 3) + 1;
+  }
+  draw(){
+    
+    ctx3.drawImage(this.Image, this.frameX * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y , this.width, this.height);
+  }
+}
+
+class Enemy4 extends Enemy{
+  constructor(){
+    super();
+    this.Image = new Image();
+    this.Image.src = './NPC enemy/enemy4.png';
+    this.spriteWidth = 158;
+    this.spriteHeight = 150;
+    this.width = this.spriteWidth / 2.5;
+    this.height = this.spriteHeight / 2.5;
+    this.WingsDifferentFly = Math.floor(Math.random() * 3) + 1;
+  }
+  draw(){
+    
+    ctx4.drawImage(this.Image, this.frameX * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y , this.width, this.height);
+  }
+}
+
 // const enemy1 = new Enemy();
 // const enemy2 = new Enemy();
 
 for (let i = 0; i < numberOfEnemies; i++){
   enemyArr.push(new Enemy());
+  enemy2Arr.push(new Enemy2());
+  enemy3Arr.push(new Enemy3());
+  enemy4Arr.push(new Enemy4());
 }
+
+canvases.forEach(element => {
+  element.width = CANVAS_WIDTH;
+  element.height = CANVAS_HEIGHT;
+});
+
+
 
 console.log(enemyArr);
 
 /*======================Funzioni============================ */
 function animate(){
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx2.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx3.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx4.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   // enemy1.update();
   // enemy1.draw();
   enemyArr.forEach(enemy => {
+    enemy.update();
+    enemy.draw();
+  });
+
+  enemy2Arr.forEach(enemy => {
+    enemy.update();
+    enemy.draw();
+  });
+
+  enemy3Arr.forEach(enemy => {
+    enemy.update();
+    enemy.draw();
+  });
+
+  enemy4Arr.forEach(enemy => {
     enemy.update();
     enemy.draw();
   });
